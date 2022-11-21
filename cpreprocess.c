d4e1e902980b3ad30034ddace38b83f84c1f386d
Hikmat Jafarli <jafarlihi@gmail.com>
2022-11-21 12:24:33 +0400

    Add stuff

--- cpreprocess.c
+++ cpreprocess.c
@@ -54,5 +54,28 @@ char *preprocess(char *input) {
 
   input = strReplace(input, "\\\n", "");
 
+  size_t inputLen = strlen(input);
+  char *line = strtok(input, "\n");
+  while (line != NULL) {
+    if (line && *line != '\0') {
+      size_t lineLen = strlen(line);
+      for (int i = 0; i < lineLen; i++) {
+        if (line[i] == '/' && line[i + 1] && line[i + 1] == '/') {
+          line[i] = ' ';
+          for (int j = i + 1; j < lineLen; j++)
+            line[j] = '\0';
+        }
+      }
+    }
+    line = strtok(NULL, "\n");
+  }
+
+  for (int i = 0; i < inputLen; i++) {
+    if (input[i] == '\0') {
+      input[i] = '\n';
+      while (input[i++] == '\0');
+    }
+  }
+
   return input;
 }
--- tests.c
+++ tests.c
@@ -20,6 +20,20 @@ int main(int argc, char *argv[]) {
   char *sample2 = "char *some\\\nthing = \"23\";";
   char *expected2= "char *something = \"23\";";
   assert(strcmp(preprocess(sample2), expected2) == 0);
+
+  char *sample3 = "int something = 23; // comment1\n"
+    "int somethingElse = 55;";
+  char *expected3 = "int something = 23;  \n"
+    "int somethingElse = 55;";
+  printf("%s\n", preprocess(sample3));
+  assert(strcmp(preprocess(sample3), expected3) == 0);
+
+  char *sample4 = "int something = 24; // comment1\n"
+    "/* block\n"
+    "comment */";
+  char *expected4= "int something = 24;  \n"
+    " ";
+  assert(strcmp(preprocess(sample4), expected4) == 0);
 }
 
 #endif
